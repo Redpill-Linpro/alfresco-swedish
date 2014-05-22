503c62a4397e5d8f138851419c32cea2a99d387a
Marcus Svensson <marcus.svensson@redpill-linpro.com>
2014-05-22 20:32:03 +0200

    Fixing of issues

--- update-sources.sh
+++ update-sources.sh
@@ -2,19 +2,14 @@
 # Download each project from CrowdIn, and package is a JAR
 # Includes the TinyMCE localization
 
-#if [ -z "$CROWDIN_USER_KEY" ]; then
-#    echo "Environment variable CROWDIN_USER_KEY must be set to your personal Crowdin api key to run this script. Set it with \"export CROWDIN_USER_KEY=yourkey\" "
-#    exit 1
-#fi 
-
-l="sv_SE"
+l="sv"
 
 rm -rf ./work/alf-i18n_${l}
 mkdir -p ./work/alf-i18n_${l}
 
 cd ./work/alf-i18n_${l}
 
-curl -L http://www.tinymce.com/i18n/download.php?download=${l} > tinymce.zip
+curl -L http://www.tinymce.com/i18n/download.php?download=sv_SE > tinymce.zip
 
 curl -L http://crowdin.net/download/project/alfresco/sv-SE.zip > alfresco.zip
 
@@ -29,25 +24,33 @@ for f in `find share -type f`; do
   mv $f ${f%.properties}_${l}.properties;
 done;
 
+for f in `find rm -type f`; do
+  mv $f ${f%.properties}_${l}.properties;
+done;
+
 #Repository translations:
 mkdir -p repo/META-INF
 touch repo/META-INF/MANIFEST.MF
 cp -rf alfresco repo/
 cp -rf rm/repo/config/alfresco/* repo/alfresco/
-rm -r alfresco rm
-
 
 #Share translations:
-mkdir -p share/alfresco
-cp -rf share/* share/alfresco/
+#mkdir -p share/alfresco
+#cp -rf share/* share/alfresco/
 cp -rf rm/share/config/alfresco/* share/alfresco/
 #TinyMCE
 mkdir -p share/META-INF/modules/editors/tiny_mce
 mv langs share/META-INF/modules/editors/tiny_mce/
 
+rm -r alfresco rm
 #Create jar
-zip -r alfresco-${l}.jar repo/META-INF repo/alfresco
-zip -r share-${l}.jar share/META-INF share/alfresco
-
+cd repo
+zip -r alfresco-${l}.jar META-INF alfresco
+mv alfresco-${l}.jar ../
+cd ..
+cd share
+zip -r share-${l}.jar META-INF alfresco
+mv share-${l}.jar ../
+cd ..
 
   
